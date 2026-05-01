25d21025e1459bcdcd9129f82ee5d8a806add4b2
Sheren-04 <notforall04@gmail.com>
2026-05-01 11:40:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT ?? 8000;
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'Hello World'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
